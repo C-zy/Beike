8da3cd5bd6946bab768025fa3ca269639f480a60
SUPERY <1900696110@qq.com>
2020-05-19 21:53:15 +0800

    合并首页，用户，登录注册，城市选择，商铺办公

---
+++ js/server.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mysql = require('mysql')
+
+// 解决跨域
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin','*'); 
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'x-requested-with, content-type'); 
+    next();
+})
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'beike'
+})
+
+connection.connect()
+// 首页数据接口
+app.get('/index', (req, res) => {
+    connection.query('select * from house_secondary', function (err, result) {
+        const obj={
+            status: 200,
+            data: result
+        }
+        res.json(obj)
+    })
+})
+// 登录数据接口
+app.get('/login', (req, res) => {
+    // console.log(req.query.phone)
+    connection.query("select * from user where phone='"+req.query.phone+"'", function (err, result) {
+        const obj={
+            status: 200,
+            data: result
+        }
+        res.json(obj)
+    })
+})
+// 注册数据接口
+app.get('/registered', (req, res) => {
+    // console.log(req.query.phone)
+    connection.query("INSERT INTO user (password, phone) VALUES ("+req.query.password+", "+req.query.phone+")", function (err, result) {
+        const obj={
+            status: 200,
+            data: result
+        }
+        res.json(obj)
+    })
+})
+// 商品办公
+app.get('/city', (req, res, next) => {
+    connection.query("select * from city_shang where num='" + req.query.num + "'", (err, result) => {
+        res.json(result)
+    })
+})
+app.get('/list', (req, res, next) => {
+    connection.query("select * from list_shang where num='" + req.query.num + "'", (err, result) => {
+        res.json(result)
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
