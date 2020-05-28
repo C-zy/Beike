a848f5f67ad3c4bf19990d60d05f134f37b897d0
曾松波 <461037704@qq.com>
2020-05-28 18:31:34 +0800

    找小区房子详情页完成

--- js/server.js
+++ js/server.js
@@ -67,7 +67,14 @@ app.get('/village', (req, res, next) => {
             res.json(result)
         })
     })
-    // 二手房
+    //找小区详情页
+app.get('/village_more', (req, res, next) => {
+    connection.query("select * from village where id='" + req.query.id + "'", (err, result) => {
+        res.json(result)
+    })
+})
+
+// 二手房
 app.get('/ershoufang', (req, res) => {
     connection.query('select * from ershoufang', function(err, result) {
         const obj = {
@@ -107,7 +114,7 @@ app.get('/star_house', (req, res) => {
 });
 // 点击收藏
 app.get('/star', (req, res) => {
-    connection.query("update ershoufang set star='1' where id='"+req.query.id+"'", function(err, result) {
+    connection.query("update ershoufang set star='1' where id='" + req.query.id + "'", function(err, result) {
         const obj = {
             status: 200,
             data: result
@@ -117,50 +124,50 @@ app.get('/star', (req, res) => {
 });
 // 列表页
 app.get('/cont', (req, res) => {
-    connection.query('select * from rent_house', function (err, rent) {
-        if (err) throw err
-          const obj={
-              status:200,
-              data:rent
-          }
-          res.json(obj)
-        console.log('111', res)
-      })
-})
-// 详情页
+        connection.query('select * from rent_house', function(err, rent) {
+            if (err) throw err
+            const obj = {
+                status: 200,
+                data: rent
+            }
+            res.json(obj)
+            console.log('111', res)
+        })
+    })
+    // 详情页
 app.get('/cont_det', (req, res) => {
-  console.log(req.query.id)
-  connection.query("select * from rent_house where id="+req.query.id, function (err, rent) {
-      if (err) throw err
-        const obj={
-            status:200,
-            data:rent
+        console.log(req.query.id)
+        connection.query("select * from rent_house where id=" + req.query.id, function(err, rent) {
+            if (err) throw err
+            const obj = {
+                status: 200,
+                data: rent
+            }
+            res.json(obj)
+            console.log('111', res)
+        })
+    })
+    //查看更多房源的数据
+app.get('/detailsy', (req, res) => {
+    connection.query('select * from rent_house limit 10', function(err, rent) {
+        if (err) throw err
+        const obj = {
+            status: 200,
+            data: rent
         }
         res.json(obj)
-      console.log('111', res)
+            // console.log('111', res)
     })
 })
-//查看更多房源的数据
-app.get('/detailsy', (req, res) => { 
-    connection.query('select * from rent_house limit 10', function (err, rent) {
+app.get('/apart', (req, res) => {
+    connection.query('select * from apartment', function(err, rent) {
         if (err) throw err
-          const obj={
-              status:200,
-              data:rent
-          }
-          res.json(obj)
-        // console.log('111', res)
-      })
-})
-app.get('/apart', (req, res) => { 
-  connection.query('select * from apartment', function (err, rent) {
-      if (err) throw err
-        const obj={
-            status:200,
-            data:rent
+        const obj = {
+            status: 200,
+            data: rent
         }
         res.json(obj)
-      console.log('111', res)
+        console.log('111', res)
     })
 })
 
